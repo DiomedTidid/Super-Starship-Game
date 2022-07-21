15a982f4267d5679a52cc7ce3f5c35c17c5949ca
Diomed <white-black@bk.ru>
2022-07-21 11:24:41 +0700

    Проект начат!

---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    static public Hero S;
+    [Header("Set in Inspector")]
+    public float speed = 30;
+    public float rollMult = -45;
+    public float pitchMult = 30;
+
+    [Header("Set Dynamically")]
+    public float shieldLevel = 1;
+    void Awake()
+    {
+        if (S == null) S = this;
+        else Debug.LogError("Hero.Awake() - second Hero");
+    }
+
+    
+    void Update()
+    {
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxisRaw("Vertical");
+
+        Vector3 pos = transform.position;
+        pos.x += xAxis * speed * Time.deltaTime;
+        pos.y += yAxis * speed * Time.deltaTime;
+        transform.position = pos;
+
+        transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * pitchMult, 0);
+
+    }
+}
